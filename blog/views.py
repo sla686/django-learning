13a26427ec9bde16da603466643c68b54e41a444
Viacheslav Semushin <sla686@gmail.com>
2022-09-06 12:40:13 +0300

    trying out a class-based view

--- blog/views.py
+++ blog/views.py
@@ -34,3 +34,13 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
